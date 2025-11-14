e294ad3d3c8ba89814b7a9df09d32bb379652e34
Fernandez Cristian <cristianexefer26@gmail.com>
2025-11-14 06:05:30 -0300

    react-router, y paginas de login y registrar

---
+++ Ejercicio C/frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-black">Iniciar Sesión</h1>
+      <p>Administra tus vehículos, conductores y viajes</p>
+    </div>
+  );
+};
+
+export default Login;
---
+++ Ejercicio C/frontend/src/pages/Registrar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Registrar = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-black">Crea tu Cuenta</h1>
+      <p>Completa el formulario para registrarte</p>
+    </div>
+  );
+};
+
+export default Registrar;
